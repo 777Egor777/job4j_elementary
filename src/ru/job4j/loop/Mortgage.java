0f96e90528fe7ceb04e57748388b4d773febbfbb
Egor <yegeraskin13@gmail.com>
2020-03-03 21:38:37 +0400

    5.7. Ипотека.[#246240]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, int percent) {
+        int result = 1;
+        double currentDebt = amount * (1 + percent / 100.0);
+        while(currentDebt > salary) {
+            result++;
+            currentDebt -= salary;
+            currentDebt *= (1 + percent / 100.0);
+        }
+        return result;
+    }
+}
