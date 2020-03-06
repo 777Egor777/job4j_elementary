8658e14628bac64b5f9abf16848970bc27fc6bc0
Egor <yegeraskin13@gmail.com>
2020-03-06 14:24:01 +0400

    6.4.1. Слово заканчивается с ...[#246178](fix1)

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -3,16 +3,13 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] postfix) {
         boolean result = true;
-        if (postfix.length > word.length) {
-            result = false;
-        } else {
-            for(int index = 0; index < postfix.length; ++index) {
-                if (postfix[index] != word[word.length - postfix.length + index]) {
-                    result = false;
-                    break;
-                }
+        for(int index = 0; index < postfix.length; ++index) {
+            if (postfix[index] != word[word.length - postfix.length + index]) {
+                result = false;
+                break;
             }
         }
+
         return result;
     }
 }
