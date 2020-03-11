6a0818c3810e236d1b30c9915d98f8086ebdd5dc
Egor <yegeraskin13@gmail.com>
2020-03-11 23:40:47 +0400

    2.2. Рефакторинг - Площадь треугольника.[#246338]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,7 +1,70 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.*;
+
+/**
+ * Calculating area of the triangle
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
-        return (ab + ac > bc) && (ab + bc > ac) && (bc + ac > ab);
+    /**
+     * Three vertux of the triangle
+     */
+    private Point firstVertex;
+    private Point secondVertex;
+    private Point thirdVertex;
+
+    /**
+     * Standart constructor
+     */
+    public Triangle(Point firstVertex, Point secondVertex, Point thirdVertex) {
+        this.firstVertex = firstVertex;
+        this.secondVertex = secondVertex;
+        this.thirdVertex = thirdVertex;
+    }
+
+    /**
+     * Method calculate area of the triangle
+     * @return area of the triangle
+     */
+    public double area() {
+        double result = -1;
+        double firstSide = this.firstVertex.distance(this.secondVertex);
+        double secondSide = this.firstVertex.distance(this.thirdVertex);
+        double thirdSide = this.thirdVertex.distance(this.secondVertex);
+        if (Triangle.exist(firstSide, secondSide, thirdSide)) {
+            double p = Triangle.semiPerimeter(firstSide, secondSide, thirdSide);
+            result = sqrt(p) * sqrt(p - firstSide) * sqrt(p - secondSide)*
+                     sqrt(p - thirdSide);
+        }
+        return result;
+    }
+
+    /**
+     * Method checks if triangle which
+     * sides length is:
+     * @param firstSide - length of the first side of the triangle
+     * @param secondSide - length of the second side of the triangle
+     * @param thirdSide - length of the third side of the triangle
+     * @return true - if triangle exist
+     *         false - if triange not exist
+     */
+    public static boolean exist(double firstSide, double secondSide, double thirdSide) {
+        return  (firstSide < secondSide + thirdSide) &&
+                (secondSide < firstSide + thirdSide) &&
+                (thirdSide < firstSide + secondSide);
+    }
+
+    /**
+     * Method return semi-perimeter of
+     * the triangle which sides length is:
+     * @param firstSide - length of the first side of the triangle
+     * @param secondSide - length of the second side of the triangle
+     * @param thirdSide - length of the third side of the triangle
+     * @return semi-perimeter
+     *         of the triangle
+     */
+    public static double semiPerimeter(double firstSide, double secondSide, double thirdSide) {
+        return (firstSide + secondSide + thirdSide) / 2;
     }
 }
--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -19,4 +19,35 @@ public class TriangleTest {
                 is(false)
         );
     }
+
+    @Test
+    public void area() {
+        Point a = new Point(0, 0);
+        Point b = new Point(3, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double out = triangle.area();
+        double expected = 6;
+        assertThat(out, is(expected));
+    }
+
+    @Test
+    public void semiPerimeter() {
+        double firstSide = 2;
+        double secondSide = 3;
+        double thirdSide = 4;
+        double out = Triangle.semiPerimeter(firstSide, secondSide, thirdSide);
+        double expected = 4.5;
+        assertThat(out, is(expected));
+    }
+
+    @Test
+    public void semiPerimeter2() {
+        double firstSide = 5;
+        double secondSide = 4;
+        double thirdSide = 7;
+        double out = Triangle.semiPerimeter(firstSide, secondSide, thirdSide);
+        double expected = 8;
+        assertThat(out, is(expected));
+    }
 }
